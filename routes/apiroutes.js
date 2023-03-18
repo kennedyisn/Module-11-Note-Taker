44d2b0fda240a27be91ba675ab6f54e07d671f8d
kennedyisn <kennedyhayles@gmail.com>
2023-03-18 15:01:55 -0400

    make it better

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,2 +1,63 @@
+const apinotes = require("express").Router();
+const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
-const path = require ('path');
+const path = require('path');
+
+// const readFromFile = (filePath) => {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(filePath, 'utf8', (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
+
+// const readAndAppend = (content, file) => {
+//   fs.readFile(file, 'utf8', (err, data) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       const parsedData = JSON.parse(data);
+//       parsedData.push(content);
+//       fs.writeFile(file, JSON.stringify(parsedData, null, 2), (writeErr) =>
+//         writeErr ? console.error(writeErr) : console.info(`Successfully updated ${file}`)
+//       );
+//     }
+//   });
+// };
+
+const noteData = fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8');
+
+const savedNotes = JSON.parse(noteData);
+console.log(savedNotes);
+
+// GET Route for retrieving api notes from db file
+apinotes.get('/', (req, res) => {
+  console.info(`${req.method} request received for api notes`);
+  res.sendFile(path.join(__dirname, '../db/db.json'));
+});
+
+// POST Route to add a new api note
+apinotes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  console.log(req.body);
+
+  if (req.body) {
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      tip_id: uuidv4(),
+    };
+
+    savedNotes.push(newNote);
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(savedNotes, null, 2));
+    res.json(`New note added successfully`);
+  } else {
+    res.status(400).send('Error in adding new note');
+  }
+});
+
+module.exports = apinotes;
--- server.js
+++ server.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const uniqid = require('uniqid')
+const apiroutes = require('./routes/apiroutes.js');
 
 const PORT = process.env.PORT || 3001;
 
 //initialize app
 const app = express();
 
+// //Middleware for parsing data
+// app.use(express.static('public'));
+
+// Middleware for parsing data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Use the apinotes router
+app.use('/api/notes', apiroutes);
+
 //GET route for notes.html 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 //GET route for index.html 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
